c6ccc90b8e8912ec8e8226701d9d2c0b08394714
atlashamzali <ahamzali12159@ada.edu.az>
2024-02-18 00:33:10 +0400

    Students table created

---
+++ students_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE STUDENTS (
+    ST_ID SERIAL PRIMARY KEY,
+    ST_NAME VARCHAR(20),
+    ST_LAST VARCHAR(20)
+);
+
+INSERT INTO STUDENTS (ST_NAME, ST_LAST) VALUES 
+    ('Konul', 'Gurbanova'),
+    ('Shahnur', 'Isgandarli'),
+    ('Natavan', 'Mammadova');
+
+SELECT*FROM STUDENTS;
+
